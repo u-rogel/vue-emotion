db5261332442a612bb25fc3f53a0f1f027c82b73
Uria Rogel <uria_rogel@hotmail.com>
2021-05-01 19:08:07 +0200

    eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    semi: [2, 'never'],
+    quotes: [2, 'single'],
+    'import/prefer-default-export': [0],
+    'linebreak-style': 0,
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['Vue'] }],
+  },
+}
